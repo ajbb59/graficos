fa97592e1a52ca3be91b57a66cc3b71ef4dc60ae
ajbb59 <48368668+ajbb59@users.noreply.github.com>
2023-04-03 11:17:08 -0500

    Update barras.php

--- barras.php
+++ barras.php
@@ -34,8 +34,8 @@ $valoresX=array("2017-01-01","2017-01-05","2017-02-12","2017-02-17","2017-03-23"
 
 <script type="text/javascript">
 
-	datosX=crearCadenaBarras('<?php echo $datosX ?>');
-	datosY=crearCadenaBarras('<?php echo $datosY ?>');
+	datosX=crearCadenaBarras();
+	datosY=crearCadenaBarras();
 
 	var data = [
 		{
